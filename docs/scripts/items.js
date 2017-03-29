bda0ee67c7a3e5011456e816464fc549a9f5b964
wolfram74 <phaugen@gmail.com>
2017-03-29 13:19:43 -0500

    added a few items

--- docs/scripts/items.js
+++ docs/scripts/items.js
@@ -6,5 +6,21 @@ const items = {
   ironPlate:{
     readable:'Iron Plate',
     ingredients:['ironOre']
-  }
+  },
+  inserter:{
+    readable:'Inserter',
+    ingredients:['ironPlate', 'ironGearWheel', 'electronicCircuit']
+  },
+  ironGearWheel:{
+    readable:'Iron Gear Wheel',
+    ingredients:['ironPlate']
+  },
+  electronicCircuit:{
+    readable:'Electronic Circuit',
+    ingredients:['ironPlate', 'copperCable']
+  },
+  copperCable:{
+    readable:'Copper Cable',
+    ingredients:['copperPlate']
+  },
 }
